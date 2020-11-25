759620e5d32de17d16557deb15e8431a15fadca3
MahmoudSiliman <modysiliman75@gmail.com>
2020-11-25 22:43:14 +0200

    create hr manage employees financ page

--- layouts/header.php
+++ layouts/header.php
@@ -133,6 +133,10 @@
 												<a href="hrManageEmployees.php" class="kt-menu__link kt-menu__toggle"> جدول إدارة الموظفين</a>
 											</li>
 
+											<li class="kt-menu__item">
+												<a href="hrManageEmployessFinac.php" class="kt-menu__link kt-menu__toggle"> إضافة حوافز او خصومات للموظفين</a>
+											</li>
+
 										</ul>
 									</div>
 								</li>
@@ -204,7 +208,7 @@
 										<li class="kt-nav__item kt-nav__item--active">
 											<a href="#" class="kt-nav__link">
 												<span class="kt-nav__link-icon"><img src="assets/media/flags/158-egypt.svg" alt="" /></span>
-												<span class="kt-nav__link-text">Arabic</span>
+												<span class="kt-nav__link-text">العربية</span>
 											</a>
 										</li>
 
@@ -224,7 +228,6 @@
 							<div class="kt-header__topbar-item kt-header__topbar-item--user">
 								<div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
 									<div class="kt-header__topbar-user">
-										<span class="kt-header__topbar-welcome kt-hidden-mobile">Hi,</span>
 										<span class="kt-header__topbar-username kt-hidden-mobile">Sean</span>
 										<img alt="Pic" class="kt-radius-100" src="assets/media/users/300_25.jpg" />
 									</div>
@@ -232,7 +235,7 @@
 								<div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
 
 									<!--START: HEAD -->
-									<div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x" style="background-image: url(assets/media/misc/bg-1.jpg)">
+									<div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x" style="background-image: url(assets/media/bg/bg-8.jpg)">
 										<div class="kt-user-card__avatar">
 											<img class="" alt="Pic" src="assets/media/users/300_25.jpg" />
 										</div>
