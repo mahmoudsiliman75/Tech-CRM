c65a4700d6690bc190716c3982bb46b5ca7e3b22
mahmoudsiliman <mahmoudsiliman75@gmail.com>
2021-01-13 16:59:26 +0200

    edit hr manage employees page

--- hrManageEmployees.php
+++ hrManageEmployees.php
@@ -24,27 +24,80 @@
             <thead class="thead-dark">
               <tr>
                 <th>#</th>
-                <th> الاسم </th>
+                <th class="text-center" style="width: 130px !important;"> الاسم </th>
                 <th>المسمي الوظيفي</th>
                 <th>البريد الإلكتروني</th>
                 <th>العنوان</th>
                 <th>رقم الهاتف</th>
-                <th>الراتب</th>
-                <th>تاريخ بداية التوظيف</th>
                 <th class="action" style="width: 85px !important;">إجراء</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td>1</td>
-                <td> محمد عيد </td>
+                <td class="text-center">1</td>
+                <td class="text-center"> 
+                  <p class="mb-1"> محمد عيد </p>
+                  <span class="kt-badge kt-badge--success kt-badge--inline kt-badge--pill kt-badge--rounded" style="height: 25px; width: 80%">
+                    مستخدم للنظام
+                  </span>
+                </td>
                 <td> Back-End Dev </td>
                 <td>mohamed@localhost.com</td>
                 <td>عنوان محمد عيد</td>
                 <td>0100000010</td>
-                <td>5000</td>
-                <td>2020 - 10 - 10</td>
                 <td align="right">
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="تاريخ العمليات">
+                    <i class="la la-calendar"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="سجل الحضور و الانصراف">
+                    <i class="la la-clock-o"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="سجل تعاقدات المشاريع">
+                    <i class="la la-file-text-o"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title=" المهام ">
+                    <i class="la la-slack"></i>
+                  </a>
+                  <a href="hrSalaryDetails.php" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="مفردات المرتب">
+                    <i class="la la-dollar font-weight-bold"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--primary" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="تعديل">
+                    <i class="la la-edit"></i>
+                  </a>
+                  <a href="#" class="archive kt-badge kt-badge--outline kt-badge--warning" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="اضافة الى الارشيف">
+                    <i class="la la-hdd-o"></i>
+                  </a>
+                  <a href="#" class="delete kt-badge kt-badge--outline kt-badge--danger" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="حذف">
+                    <i class="la la-trash"></i>
+                  </a>
+                </td>
+              </tr>
+
+              <tr>
+                <td class="text-center">2</td>
+                <td class="text-center"> 
+                  <p class="mb-1"> محمود سليمان </p>
+                  <span class="kt-badge kt-badge--danger kt-badge--inline kt-badge--pill kt-badge--rounded" style="height: 25px; width: 80%">
+                    غير مستخدم للنظام
+                  </span>
+                </td>
+                <td> Front-End Dev </td>
+                <td>siliman@localhost.com</td>
+                <td>عنوان محمود سليمان</td>
+                <td>0100000011</td>
+                <td align="right">
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="تاريخ العمليات">
+                    <i class="la la-calendar"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="سجل الحضور و الانصراف">
+                    <i class="la la-clock-o"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="سجل تعاقدات المشاريع">
+                    <i class="la la-file-text-o"></i>
+                  </a>
+                  <a href="#" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title=" المهام ">
+                    <i class="la la-slack"></i>
+                  </a>
                   <a href="hrSalaryDetails.php" class="kt-badge kt-badge--outline kt-badge--success" data-skin="dark" data-toggle="kt-tooltip" data-placement="top" title="مفردات المرتب">
                     <i class="la la-dollar font-weight-bold"></i>
                   </a>
